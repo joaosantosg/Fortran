b699c8137f957e7d315dd12dba22ef3631e1b6c2
João Victor <50639616+joaosantosg@users.noreply.github.com>
2024-02-14 23:32:19 -0300

    Update age.f90

--- basics/age.f90
+++ basics/age.f90
@@ -3,7 +3,7 @@ implicit none
 
 integer :: user_age
 
-print *, "Please tell me your are"
+print *, "Please tell me your age and I will predict your age"
 read(*,*) user_age
 
 print *, "Yor age is:", user_age
